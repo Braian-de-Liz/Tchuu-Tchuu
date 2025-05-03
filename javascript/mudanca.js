3cae11b7c1c4cb147a11f6e283bf6e30223823bd
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-05-03 18:28:37 -0300

    HAHAHHAHAAH

---
+++ javascript/mudanca.js
@@ -0,0 +1,9 @@
+function au(){
+    let p = document.getElementsByClassName("container");
+    p.append('augosto é gay <br>');
+}
+
+function gui(){
+    let p = document.getElementById("Registro");
+    p.append('fasfefasfefesfseaf');
+}
