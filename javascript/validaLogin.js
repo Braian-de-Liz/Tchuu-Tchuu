35ec4a2282288a46dae639bde2f51a88346048e4
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-06-14 22:13:07 -0300

    validação de Login Pronta

---
+++ javascript/validaLogin.js
@@ -0,0 +1,32 @@
+function ValidaLogin(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const senha = document.getElementById('senha').value.trim();
+
+    if (!email || !senha) {
+        alert("Campos não preenchidos");
+        return false;
+    }
+
+    function ValidaEmail(email) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    }
+
+    if (!ValidaEmail(email)) {
+        alert('Email inválido');
+        return false;
+    }
+
+    if (senha.length < 8) {
+        alert("A senha deve ter pelo menos 8 caracteres.");
+        return false;
+    }
+
+    document.getElementById('passou').innerHTML = "Seu login foi autorizado";
+
+
+
+    return true;
+}
