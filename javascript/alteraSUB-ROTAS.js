73f6d14f70ec0376aeeb7c56e7f31309eda17f09
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-06-17 14:03:53 -0300

    teste

--- javascript/alteraSUB-ROTAS.js
+++ javascript/alteraSUB-ROTAS.js
@@ -1,9 +1,12 @@
 function mostrarConteudo(id) {
-      const todos = document.querySelectorAll('.conteudo');
-      todos.forEach(el => el.style.display = 'none');
-      const alvo = document.getElementById(id);
-      if (alvo) alvo.style.display = 'block';
+  const todos = document.querySelectorAll('.conteudo');
+  todos.forEach(el => el.style.display = 'none');
+  const alvo = document.getElementById(id);
+  if (alvo) alvo.style.display = 'block';
 
-      // Se for o painel de análise, renderiza o gráfico
-      if (id === 'painel-analise') desenharGrafico();
-    }
+
+  if (id === 'painel-analise') desenharGrafico();
+}
+
+
+
