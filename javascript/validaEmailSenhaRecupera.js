b9597354bb239dbfa556819bb3a52ed46355b2fb
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-06-23 10:16:33 -0300

    fofofoof

--- javascript/validaEmailSenhaRecupera.js
+++ javascript/validaEmailSenhaRecupera.js
@@ -4,22 +4,33 @@ function validadeEmail(email) {
     return regex.test(email);
 }
 
+function ValidaNumRegistro(RegistroFun) {
+    return /^\d{12}$/.test(RegistroFun);
+}
 
 function ValidaSenhaRecuperacao(event){
     event.preventDefault();
 
 
     const email = document.getElementById('email').value.trim();
+    const NumFun = document.getElementById('Numfun').value.trim();
 
-
+    
     if(!validadeEmail(email)){
         alert('Email Inválido, tente novamente');
         return false;
     }
 
+    if(!ValidaNumRegistro(NumFun)){
+        alert("Seu número de Funcionário ");
+        return false;
+    }
 
-    alert('Email Identificado com Sucesso');
 
+
+    
+
+    alert('Dados Aprovados com Sucesso');
     document.getElementById('passou').innerHTML = ('Funcionou');
 
     return true
