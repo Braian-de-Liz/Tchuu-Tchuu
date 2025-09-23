f0694251f649056120ee01297df7d14712e9c797
Braian-de-Liz <delizbraian@gmail.com>
2025-09-22 21:53:41 -0300

    mundanças talvez necessárias com mudança de planos para postgre sql

--- BACK-END_NODEJS/src/databases/conectar_banco.js
+++ BACK-END_NODEJS/src/databases/conectar_banco.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql2/promise';
+/* import mysql from 'mysql2/promise';
 
 async function conectar() {
   const conexao = await mysql.createConnection({
@@ -13,3 +13,25 @@ async function conectar() {
 }
 
 export { conectar };
+
+
+// -------------------> SEPARAÇÃO DE OPÇÕES <----------------------
+
+
+import { Client } from 'pg';
+
+async function conectar() {
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: true }
+  });
+
+  await client.connect();
+  return client;
+}
+
+export { conectar }; */
