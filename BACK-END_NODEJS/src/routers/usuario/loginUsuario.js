e8eb5ea61255dfb25a2a530d31c2a2e17e35e0e2
Braian-de-Liz <delizbraian@gmail.com>
2025-09-23 22:20:45 -0300

    inicio da API de login

--- BACK-END_NODEJS/src/routers/usuario/loginUsuario.js
+++ BACK-END_NODEJS/src/routers/usuario/loginUsuario.js
@@ -1,1 +1,11 @@
 import { Router } from "express";
+import { conectar } from "../../databases/conectar_banco";
+
+const router = Router();
+
+router.post('/usu_login', async (req, res) => {
+    
+});
+
+
+export {router};
