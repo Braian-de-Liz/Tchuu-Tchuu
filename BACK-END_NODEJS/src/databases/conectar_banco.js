35b0497a46c2f478f3e18e38fad936a3d98d4603
Braian-de-Liz <delizbraian@gmail.com>
2025-09-25 15:19:10 -0300

    Váriaveis de ambiente nas credênciais de DB

--- BACK-END_NODEJS/src/databases/conectar_banco.js
+++ BACK-END_NODEJS/src/databases/conectar_banco.js
@@ -1,6 +1,23 @@
 // src/databases/conectar_banco.js
 import { Client } from 'pg';
 
+
+async function conectar() {
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: true }
+  });
+  
+  await client.connect();
+  return client;
+}
+
+export { conectar };
+
 /* async function conectar() {
     const client = new Client({
         host: 'ep-cold-tooth-adchk7yu-pooler.c-2.us-east-1.aws.neon.tech',
@@ -22,19 +39,3 @@ export { conectar };
 
 
 import { Client } from 'pg'; */
-
-async function conectar() {
-  const client = new Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    ssl: { rejectUnauthorized: true }
-  });
-
-  await client.connect();
-  return client;
-}
-
-export { conectar };
