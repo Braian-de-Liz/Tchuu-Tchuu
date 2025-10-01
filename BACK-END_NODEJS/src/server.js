f2f8e9dee98f78913bf461e05b1d5a6bd31baac5
Braian-de-Liz <delizbraian@gmail.com>
2025-10-01 08:03:54 -0300

    tentativa de correção

--- BACK-END_NODEJS/src/server.js
+++ BACK-END_NODEJS/src/server.js
@@ -40,6 +40,7 @@ app.get('/acordar', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+// server;listen(PORT);
 
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
