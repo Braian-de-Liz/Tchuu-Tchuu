07ab083267e71775c2b0d7292144316e39f6682c
Braian-de-Liz <delizbraian@gmail.com>
2025-10-03 12:20:18 -0300

    formatação

--- BACK-END_NODEJS/src/routers/trens/registrar_trem.js
+++ BACK-END_NODEJS/src/routers/trens/registrar_trem.js
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import bycrypt from 'bcrypt';
 import { conectar } from '../../databases/conectar_banco.js';
 
@@ -6,24 +6,24 @@ const router = Router();
 
 
 router.post("/trem_Cadastro", async (req, res) => {
-    
-    
-    console.log("registrando trem");
-    const {nomeTrem, numero_de_Trem, fabricante, cpfUser, dataRegistro, NomeUser } = req.body;
 
-    if(!nomeTrem ||!numero_de_Trem || !fabricante || !cpfUser || !dataRegistro || !NomeUser ){
+
+    console.log("registrando trem");
+    const { nomeTrem, numero_de_Trem, fabricante, cpfUser, dataRegistro, NomeUser } = req.body;
+
+    if (!nomeTrem || !numero_de_Trem || !fabricante || !cpfUser || !dataRegistro || !NomeUser) {
         return res.status(400).json({
-            status:'erro',
+            status: 'erro',
             mensagem: 'Preencha  todos os campos'
         });
     };
 
-const cpfTRUE = cpf.replace(/[^\d]/g, '');
+    const cpfTRUE = cpf.replace(/[^\d]/g, '');
 
     if (cpfTRUE.length !== 11) {
         return res.status(400).json({ status: 'erro', mensagem: 'CPF inválido.' });
     }
-    
+
 
 });
 
