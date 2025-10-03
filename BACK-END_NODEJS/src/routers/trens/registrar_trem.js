37f3ff5a36c1b029aa3d2fe463fe84b9012f7fb3
Guilherme-JSS <schutzgui2707@gmail.com>
2025-10-03 12:15:51 -0300

    checagem resgistro trem

--- BACK-END_NODEJS/src/routers/trens/registrar_trem.js
+++ BACK-END_NODEJS/src/routers/trens/registrar_trem.js
@@ -11,6 +11,20 @@ router.post("/trem_Cadastro", async (req, res) => {
 
     const {nomeTrem, numero_de_Trem, fabricante, cpfUser, dataRegistro, NomeUser } = req.body;
 
+    if(!nomeTrem ||!numero_de_Trem || !fabricante || !cpfUser || !dataRegistro || !NomeUser ){
+        return res.status(400).json({
+            status:'erro',
+            mensagem: 'Preencha  todos os campos'
+        });
+    };
+
+const cpfTRUE = cpf.replace(/[^\d]/g, '');
+
+    if (cpfTRUE.length !== 11) {
+        return res.status(400).json({ status: 'erro', mensagem: 'CPF inválido.' });
+    }
+    
+
 });
 
 
