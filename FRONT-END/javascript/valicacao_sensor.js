922db63faf29b2a072ca5eddc6d1dd1bb7edef7b
Braian-de-Liz <delizbraian@gmail.com>
2025-10-03 22:52:00 -0300

    validação básica com o cadastro de sensor

--- FRONT-END/javascript/valicacao_sensor.js
+++ FRONT-END/javascript/valicacao_sensor.js
@@ -3,9 +3,39 @@ const registrar = document.getElementById("registrar");
 async function validar_e_conectar(e) {
     e.preventDefault();
 
-    
-    
+    const tipo_sensor = document.getElementById("TipoSensor").value;
+    const marca_sensor = document.getElementById("marca_sensor").value;
+    const data = document.getElementById("Data_cadast").value
+
+    if (!tipo_sensor || !marca_sensor || !data) {
+        alert("preencha os dados");
+        return false;
+    }
+
+
+    const [ano, mes, dia] = data.split('-').map(Number);
+
+    if (isNaN(dia) || isNaN(mes) || isNaN(ano)) {
+        alert("Data inválida.");
+        return false;
+    }
+
+    if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || ano < 2025) {
+        alert("Data de nascimento inválida.");
+        return false;
+    }
+
+    class sensor {
+        constructor(tipo_sensor, marca_sensor, data) {
+            this.tipo_sensor = tipo_sensor,
+                this.marca_sensor = marca_sensor,
+                this.data = data
+        }
+    }
+
+    const sensor_novo = new sensor(tipo_sensor, marca_sensor, data);
+
 }
 
 
-registrar.addEventListener("click", );
+registrar.addEventListener("click", validar_e_conectar());
