6b6b07f9742377f9d6a2681ce044282d0390ebae
Braian-de-Liz <delizbraian@gmail.com>
2025-10-06 20:35:40 -0300

    query rota delete trem

--- BACK-END_NODEJS/src/routers/trens/deletar_trem.js
+++ BACK-END_NODEJS/src/routers/trens/deletar_trem.js
@@ -4,12 +4,30 @@ import { conectar } from "../../databases/conectar_banco.js";
 const router = Router();
 
 router.delete("/trem/:cpf_user", async (req, res) => {
-    
-    const {cpf_user} = req.params;
+
+    const { cpf_user } = req.params;
 
     if (!cpf_user || cpf_user !== 11) {
-        
+        return res.status(400).json({
+            status: 'erro',
+            mensagem: 'CPF inválido. Deve conter 11 dígitos.'
+        });
+
     }
+
+
+    let db;
+
+    try {
+        
+        db = await conectar();
+        const consulta =  await db.query("", [])
+    }
+    catch {
+
+    }
+
+
 
 
     
