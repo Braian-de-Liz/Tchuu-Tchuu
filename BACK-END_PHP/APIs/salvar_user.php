afe1ef63cafbe982e798db366220d5272d8ddd02
Braian de Liz da Silva <delizbraian@gmail.com>
2025-10-15 12:18:38 -0300

    limpar as impurezas

--- BACK-END_PHP/APIs/salvar_user.php
+++ BACK-END_PHP/APIs/salvar_user.php
@@ -1,11 +1,3 @@
 <?php
 
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Content-Type');
-
-
-
-
 ?> 
