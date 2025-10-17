26f45deb3bf26c16a8e899c77bb38c5a546f133e
Braian de Liz da Silva <delizbraian@gmail.com>
2025-10-17 14:52:11 -0300

    corrigido

--- FRONT-END/javascript/containerdemanutenção.js
+++ FRONT-END/javascript/containerdemanutenção.js
@@ -1,17 +1,16 @@
-document.querySelectorAll('.menu-mini').forEach(botao => {
-  
-  botao.addEventListener('click', function () {
-    const item = this.closest('.item');
-    const expansivel = item.querySelector('.conteudo-expansivel');
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.menu-mini').forEach(botao => {
+    botao.addEventListener('click', function () {
+      const item = this.closest('.item');
+      const expansivel = item.querySelector('.conteudo-expansivel');
 
+      document.querySelectorAll('.conteudo-expansivel').forEach(el => {
+        if (el !== expansivel) {
+          el.classList.remove('aberto');
+        }
+      });
 
-    document.querySelectorAll('.conteudo-expansivel').forEach(el => {
-      if (el !== expansivel) {
-        el.classList.remove('aberto');
-      }
+      expansivel.classList.toggle('aberto');
     });
-
-
-    expansivel.classList.toggle('aberto');
   });
 });
