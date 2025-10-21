20ca70c2a2823ec0f79bed343ee2cf13a35a9ead
Braian-de-Liz <delizbraian@gmail.com>
2025-10-21 10:03:57 -0300

    async function

--- FRONT-END/javascript/mostrar_dados_trens.js
+++ FRONT-END/javascript/mostrar_dados_trens.js
@@ -1,14 +1,5 @@
-// const div = document.createElement("div");
-
 async function mostrar_dados_trem() {
 
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-        alert("usuário não logado, Como você entrou? se for bonzinho me conte no email delizbraian@gmail.com");
-        window.location.href = "../index.html";
-        return;
-    }
 
     try {
 
@@ -27,37 +18,31 @@ async function mostrar_dados_trem() {
         console.log("Dados recebidos:", info);
 
         if (resposta.ok) {
-            // const usuario = info.usuario;
-            if (resposta.ok) {
-                const dados = await resposta.json();
+            const usuario = info.usuario;
 
-                /*             nome_campo.textContent = usuario.nome || 'Carregando...';
-                            data_nasc_campo.textContent = usuario.data_nasc ? new Date(usuario.data_nasc).toLocaleDateString('pt-BR') : 'Carregando...';
-                            email_campo.textContent = usuario.email || 'Carregando...';
-                            cpf_campo.textContent = usuario.cpf || 'Carregando...';
-                
-                            nomeUser.textContent = usuario.nome || 'Usuário';
-                
-                            localStorage.setItem('usuarioNome', usuario.nome || 'Usuário'); */
+            /*             nome_campo.textContent = usuario.nome || 'Carregando...';
+            data_nasc_campo.textContent = usuario.data_nasc ? new Date(usuario.data_nasc).toLocaleDateString('pt-BR') : 'Carregando...';
+            email_campo.textContent = usuario.email || 'Carregando...';
+            cpf_campo.textContent = usuario.cpf || 'Carregando...';
+            
+            nomeUser.textContent = usuario.nome || 'Usuário';
+            
+            localStorage.setItem('usuarioNome', usuario.nome || 'Usuário'); */
 
-            } else {
+        } else {
 
-                alert('Erro: ' + info.mensagem);
-                localStorage.removeItem('token');
-                window.location.href = "../index.html";
-            }
+            alert('Erro: ' + info.mensagem);
+            localStorage.removeItem('token');
+            window.location.href = "../index.html";
         }
-
     }
 
 
     catch (erro) {
 
-        }
-
-    
-    };
+    }
 
 
+};
 
-    document.addEventListener("DOMContentLoaded", mostrar_dados_trem);
+document.addEventListener("DOMContentLoaded", mostrar_dados_trem);
