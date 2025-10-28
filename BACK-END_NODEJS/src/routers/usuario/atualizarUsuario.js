c0dc7b0ed85d50e9c22db4cbaacaea1a46bf1802
Braian-de-Liz <delizbraian@gmail.com>
2025-10-28 15:32:08 -0300

    alteração de usuario

--- BACK-END_NODEJS/src/routers/usuario/atualizarUsuario.js
+++ BACK-END_NODEJS/src/routers/usuario/atualizarUsuario.js
@@ -10,6 +10,7 @@ router.patch("/usuario", async (req, res) => {
     const { email, senha, id } = req.body;
 
     if (!id) {
+        console.log("o id não foi enviado na requisição")
         return res.status(400).json({
             status: 'erro',
             mensagem: 'ID do usuário é obrigatório.'
@@ -17,6 +18,7 @@ router.patch("/usuario", async (req, res) => {
     }
 
     if (!email && !senha) {
+        console.log("requisição vazia");
         return res.status(400).json({
             status: 'erro',
             mensagem: 'Pelo menos um campo (email ou senha) deve ser fornecido.'
@@ -28,6 +30,7 @@ router.patch("/usuario", async (req, res) => {
 
     try {
         db = await conectar();
+        console.log("banco de dados conectado");
 
         const consulta = await db.query("SELECT email, senha FROM usuarios WHERE id = $1", [id]);
 
@@ -39,28 +42,53 @@ router.patch("/usuario", async (req, res) => {
         }
 
 
-        await db.query("ALTER TABLE usuarios COLUNM email, senha = $1, 2$", [email, senha]);
+        let query = 'UPDATE usuarios SET ';
+        const params = [];
+        let paramIndex = 1;
+
+
+        if (email) {
+            query += `email = $${paramIndex}`;
+            params.push(email);
+            paramIndex++;
+        }
+
+        if (senha) {
+            const senha_segura = await bcrypt.hash(senha, 10);
+            query += `senha = $${senha_segura}`;
+            params.push(senha_segura);
+            paramIndex++;
+        }
 
 
 
-        res.status(200).json({
-            status: "sucesso",
-            mensagem: "usuario alterado"
-        })
+        query = query.slice(0, -2);
+        query += ` WHERE id = $${paramIndex}`;
+        params.push(id);
+
+        await db.query(query, params);
+        console.log("Query monstado e enviado ao banco para consulta");
+
+        res.json({
+            status: 'sucesso',
+            mensagem: 'Dados atualizados com sucesso!'
+        });
 
 
-    }
-    catch (erro) {
-        console.error('Erro ao buscar dados do usuário:', erro);
+
+    } catch (erro) {
+        console.error('Erro ao atualizar dados do usuário:', erro);
         res.status(500).json({
             status: 'erro',
-            mensagem: 'Erro interno do servidor.'
+            mensagem: 'Erro interno do servidor ao tentar atualizar os dados.'
         });
-    }
 
-    finally{
-        await db.end();
-        console.log("conexão encerrada");
+
+    } finally {
+        if (db) {
+            await db.end();
+            console.log("conexão com o banco finalizada.")
+        }
     }
 });
 
