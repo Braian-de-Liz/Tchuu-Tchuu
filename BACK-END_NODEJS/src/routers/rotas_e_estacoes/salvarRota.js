97f74bd8d468033f2dd342e74393deff41cbaf23
Braian-de-Liz <delizbraian@gmail.com>
2025-10-30 08:13:20 -0300

    correção de importação

--- BACK-END_NODEJS/src/routers/rotas_e_estacoes/salvarRota.js
+++ BACK-END_NODEJS/src/routers/rotas_e_estacoes/salvarRota.js
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import { conectar } from "../../databases/conectar_banco";
 
-import { Router } from "express";
-import { conectar } from "../../databases/conectar_banco";
 
 const router = Router();
 
