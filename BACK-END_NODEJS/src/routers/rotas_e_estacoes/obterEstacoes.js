05afbe452ca7cbeeeb7369d6947a0cd2715426f5
Braian-de-Liz <delizbraian@gmail.com>
2025-11-05 09:46:32 -0300

    rota de GET das estações

--- BACK-END_NODEJS/src/routers/rotas_e_estacoes/obterEstacoes.js
+++ BACK-END_NODEJS/src/routers/rotas_e_estacoes/obterEstacoes.js
@@ -4,7 +4,47 @@ import { conectar } from "../../databases/conectar_banco.js";
 const router = Router();
 
 router.get("/estacoes", async (req, res) => {
+    const authHeader = req.header.authorizations;
+    const token = authHeader && authHeader.split(' ')[1];
 
+    if (!token) {
+        return res.status(401).json({
+            status: 'não está logado',
+            menssage: 'tolken não recebido'
+        });
+    }
+
+    let idUsuarioLogado;
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        idUsuarioLogado = decoded.id;
+    } catch (erro) {
+        return res.status(401).json({
+            status: 'erro',
+            mensagem: 'Token inválido ou expirado.'
+        });
+    }
+
+    let db
+    try {
+        db = await conectar();
+
+        const consulta = `SELECT id, nome, endereco, latitude, longitude, cidade, estado, data_criacao
+            FROM estacoes
+            ORDER BY nome
+        `;
+
+        db.query(consulta);
+    }
+
+    catch (erro) {
+
+    }
+
+    finally{
+        console.log(`fechar conexão com o banco de dados`);
+        await db.end();
+    }
 });
 
 export default router;
---
+++ BACK-END_NODEJS/src/routers/sensores/alterarSensor.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { conectar } from "../../databases/conectar_banco.js";
+
+
+const router = Router();
+
+router.patch("", async (req, res) => {});
+
+
+export default router;
